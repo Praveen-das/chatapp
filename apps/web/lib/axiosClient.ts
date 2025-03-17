f1986ad0cab4501588886a0e38a0e738326e51b3
Praveen Das <praveendask97@gmail.com>
2025-03-18 01:56:52 +0530

    bugfix4

--- apps/web/lib/axiosClient.ts
+++ apps/web/lib/axiosClient.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API;
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
+console.log(baseURL)
+//  || 'http://localhost:3001';
 const axiosClient = axios.create({ baseURL, headers: { "Content-Type": "application/json" } });
 
 export default axiosClient;
