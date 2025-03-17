4b2702b64abb33892a7bfdda9bc32021415c4081
Praveen Das <praveendask97@gmail.com>
2025-03-18 02:29:21 +0530

    debugging1

--- apps/web/lib/axiosClient.ts
+++ apps/web/lib/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
-console.log(baseURL)
+console.log({baseURL})
 //  || 'http://localhost:3001';
 const axiosClient = axios.create({ baseURL, headers: { "Content-Type": "application/json" } });
 
