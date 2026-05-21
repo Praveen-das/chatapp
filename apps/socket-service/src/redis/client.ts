e1d351b78b2f568c0c867578409ed77b75df17c6
Praveen das <praveendask97@gmail.com>
2026-05-22 04:39:44 +0530

    5465121

--- apps/socket-service/src/redis/client.ts
+++ apps/socket-service/src/redis/client.ts
@@ -1,11 +1,10 @@
 import Redis from 'ioredis'
 
-const client = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-});
+if (!process.env.REDIS_SERVICE_URI) {
+  throw new Error("REDIS_SERVICE_URI is not defined")
+}
+
+const client = new Redis(process.env.REDIS_SERVICE_URI);
 
 client.on('error', err => {
   console.log('Redis subClient Error', err)
