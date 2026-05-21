23af62ea6a5395ddae675b73833a64e1291354fa
Praveen das <praveendask97@gmail.com>
2026-05-22 01:58:21 +0530

    feat: update Redis client

--- apps/socket-service/src/redis/client.ts
+++ apps/socket-service/src/redis/client.ts
@@ -1,16 +1,11 @@
 import Redis from 'ioredis'
 
-const client = new Redis({
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-  });
+const client = new Redis(process.env.REDIS_SERVICE_URI!);
 
 client.on('error', err => {
-    console.log('Redis subClient Error', err)
-    client.disconnect()
-    console.log('Client disconnected')
+  console.log('Redis subClient Error', err)
+  client.disconnect()
+  console.log('Client disconnected')
 });
 
 export default client
