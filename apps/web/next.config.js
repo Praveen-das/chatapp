70a824a746a9d4e797d8123196814af0c96650c7
Praveen das <praveendask97@gmail.com>
2026-06-07 07:56:19 +0530

    feat: configure next.config.js with rewrites, server actions, SVGR loader, and remote image patterns

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,14 @@
 module.exports = {
   transpilePackages: ["@repo/ui"],
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: "/api/external/:path*",
+        destination: "http://gateway-alb-313359290.ap-south-1.elb.amazonaws.com*",
+      },
+    ];
+  },
   experimental: {
     serverActions: {
       allowedOrigins: ["localhost:3000", "qw8d3s7n-3000.inc1.devtunnels.ms"],
